9cc6232e5f10ef8598328238ae41d6d235d0b54f
Andrew Kolb <akolb87@gmail.com>
2018-12-07 21:09:54 -0600

    notations and questions

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+// note to self (nts): always include express framework to receive the request and response objects
 const express = require("express");
+// initialize middlewar "morgan" for console logging
 const morgan = require("morgan");
 
+// ------ask------ you need to import `blogPostsRouter` router and route
 const bprouter = require("./blogPostsRouter");
 
 const app = express();
 
 app.use(morgan("common"));
 app.use(express.json());
+// ------ask------
 
-// you need to import `blogPostsRouter` router and route
+
 
 // requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
 app.use('/blog-posts', bprouter);
 
+// ------ask------
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
